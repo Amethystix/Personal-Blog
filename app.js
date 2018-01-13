bdea7259e8834293f629de432640eb763c8c7b70
lad475 <lad475@nyu.edu>
2018-01-13 16:03:51 -0500

    Setup the layout and rest of app.js

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ reqire('./db');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const mongoose = require('mongoose');
+
 
 const path = require('path');
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'hbs');
+
+app.get('/home', (req, res)=>{
+	res.render('home');
+});
+app.get('/blog', (req, res)=>{
+	res.render('blog');
+});
+app.get('/resume', (req, res)=>{
+	res.render('resume');
+});
+
+app.listen(3000);
